6a06cac7c6e6a416eccc901e4cd43ba391478f8f
Danylo Burmei <46345413+dan1aa@users.noreply.github.com>
2021-11-02 22:56:30 +0200

    Delete models/public directory

--- models/public/search.js
+++
@@ -1,6 +0,0 @@
-const searchButtonForm = document.querySelector(".search_form")
-const searchInput = document.querySelector('.find_input');
-
-searchButtonForm.onsubmit = function() {
-    this.setAttribute('action', `/${searchInput.value}`)
-}
